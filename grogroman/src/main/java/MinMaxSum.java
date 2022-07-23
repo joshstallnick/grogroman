cb9cbeb06035707e6b8ead10db6f9b6a118991d5
josh.stallnick <stallnick.accounts@protonmail.com>
2022-07-23 15:31:14 -0500

    MinMaxSum

--- grogroman/src/main/java/MinMaxSum.java
+++ grogroman/src/main/java/MinMaxSum.java
@@ -7,8 +7,24 @@ public class MinMaxSum {
      *
      * The function accepts INTEGER_ARRAY arr as parameter.
      */
-    public static void miniMaxSum(List<Integer> arr) {
-        // Write your code here
+    public static MinMax run(List<Integer> arr) {
+        List<Integer> sorted =
+            arr
+                .stream()
+                .sorted()
+                .toList();
 
+        Long sum =
+            sorted
+                .stream()
+                .mapToLong(it -> it)
+                .sum();
+
+        long min = sum - sorted.get(arr.size() - 1);
+        long max = sum - sorted.get(0);
+
+        return new MinMax(min, max);
     }
+
+    public record MinMax(long min, long max) {}
 }
--- grogroman/src/test/java/MinMaxSumTest.java
+++ grogroman/src/test/java/MinMaxSumTest.java
@@ -1,4 +1,7 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 /**
  * <a href="https://www.hackerrank.com/challenges/one-week-preparation-kit-mini-max-sum">Hackerrank</a>
@@ -7,6 +10,18 @@ public class MinMaxSumTest {
 
     @Test
     public void generalTest() {
+        //given
+        List<Integer> list = List.of(7, 69, 2, 221, 8974);
 
+        //299 9271
+        long min = 299;
+        long max = 9271;
+
+        //when
+        MinMaxSum.MinMax actual = MinMaxSum.run(list);
+
+        //then
+        Assertions.assertEquals(min, actual.min());
+        Assertions.assertEquals(max, actual.max());
     }
 }
