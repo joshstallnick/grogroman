a4629035dd60728396d096e491e9d5c9b0a60ac4
josh.stallnick <stallnick.accounts@protonmail.com>
2022-07-23 15:56:05 -0500

    TimeConversion

---
+++ grogroman/src/main/java/TimeConversion.java
@@ -0,0 +1,20 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+public class TimeConversion {
+
+    public static String run(String input) {
+        DateTimeFormatter inbound = DateTimeFormatter.ofPattern(INBOUND_PATTERN);
+
+        DateTimeFormatter outbound = DateTimeFormatter.ofPattern(OUTBOUND_PATTERN);
+
+        return LocalTime
+            .parse(input, inbound)
+            .format(outbound);
+    }
+
+    private static final String INBOUND_PATTERN = "hh:mm:ssa";
+    private static final String OUTBOUND_PATTERN = "HH:mm:ss";
+}
---
+++ grogroman/src/test/java/TimeConversionTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TimeConversionTest {
+
+    @Test
+    public void upperBounds() {
+        //given
+        String input = "11:59:59PM";
+        String expected = "23:59:59";
+
+        //expect
+        sharedTest(input, expected);
+    }
+
+    @Test
+    public void lowerBounds() {
+        //given
+        String input = "12:00:00AM";
+        String expected = "00:00:00";
+
+        //expect
+        sharedTest(input, expected);
+    }
+
+    private void sharedTest(String input, String expected) {
+        //when
+        String actual = TimeConversion.run(input);
+
+        //then
+        Assertions.assertEquals(expected, actual);
+    }
+}
