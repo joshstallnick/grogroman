33a26f8af145b505025d1b1a403cb573c3157d78
josh.stallnick <stallnick.accounts@protonmail.com>
2022-07-23 16:07:23 -0500

    FindMedian

---
+++ grogroman/src/main/java/FindMedian.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FindMedian {
+
+    public static int run(List<Integer> input) {
+        List<Integer> sorted =
+            input
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
+
+        int middleIndex = input.size() / 2;
+
+        return sorted.remove(middleIndex);
+    }
+}
---
+++ grogroman/src/test/java/FindMedianTest.java
@@ -0,0 +1,22 @@
+import com.sun.source.tree.BinaryTree;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class FindMedianTest {
+
+    @Test
+    public void generalTest() {
+        //given
+        List<Integer> input = List.of(144, 89, 13, 987, 577, 5, 233);
+
+        int expected = 144;
+
+        //when
+        int actual = FindMedian.run(input);
+
+        //then
+        Assertions.assertEquals(expected, actual);
+    }
+}
